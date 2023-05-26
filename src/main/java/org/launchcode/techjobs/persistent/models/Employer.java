6654af20a922f65029f5253e02b0ff7e5f7d95cd
marycross3 <marycradick@gmail.com>
2023-05-25 20:00:37 -0500

    working on test task two

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,26 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotNull
+    @Size(min=3, message = "Must be at least 3 characters.")
+    private String location;
 
+    public Employer(String location) {
+        this.location = location;
+    }
+    public Employer(){}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
