2b1306e825f5be12a0a6706c387030b5ec168c2d
marycross3 <marycradick@gmail.com>
2023-06-03 12:51:45 -0500

    before rebuild

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -2,6 +2,7 @@ package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.Skill;
 import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
 import org.launchcode.techjobs.persistent.models.data.JobRepository;
 import org.launchcode.techjobs.persistent.models.data.SkillRepository;
@@ -60,7 +61,11 @@ public class HomeController {
         }
         Optional<Employer> result = employerRepository.findById(employerId);
         if (result.isEmpty()){
+        
+            return "add";
         } else {
+            List<Skill> skillObjs = (List<Skill>) skillRepository.findAllById(skills);
+            newJob.setSkills(skillObjs);
             Employer employer = result.get();
             newJob.setEmployer(employer);
             model.addAttribute("job", employer.getName());
